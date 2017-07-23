da4ddd4bbf30cb6af03100e743234bf7c5f76db2
佐々木千明 <c.sasakitn@gmail.com>
2017-07-23 17:42:28 +0900

    メソッド。戻り値２提出

---
+++ CampChallenge_java/011.メソッド/WebApplication4_1/src/java/org/modoriti/NewServlet.java
@@ -0,0 +1,91 @@
+/*
+1. 戻り値として、人物のID、名前、生年月日、住所を配列にして返却するメソッドを作成してください。
+2. 作成したメソッドを呼び出し、戻り値のID以外を表示してください。
+ */
+package org.modoriti;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sasakichiaki
+ */
+public class NewServlet extends HttpServlet {
+
+    //自分のプロフィール(名前、生年月日、自己紹介)を3行に分けて表示するユーザー定義メソッドを作り、メソッドを10回呼び出して下さい。
+    private String profile(String id,String name,String ago,String introduction) {
+
+        return name+"<br>"+ago+"<br>"+introduction;
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            String[] profile = new String[4];
+            profile[0] = "id";
+            profile[1] = "名前";
+            profile[2] = "生年月日";
+            profile[3] = "自己紹介";
+
+            for (int i = 0; i < 10; i++) {
+                 out.print(profile(profile[0],profile[1],profile[2],profile[3]));
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
